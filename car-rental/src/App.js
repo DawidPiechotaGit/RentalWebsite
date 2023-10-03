0f230b9827ad01f32b97d3dbeb1d8c03cea42aa1
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-03 19:21:22 +0200

    Fresh start

--- car-rental/src/App.js
+++ car-rental/src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path={"/home"} element={<Home />} />
+      <Route path={"/about"} element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
